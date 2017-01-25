d8a45f176b35c4cab9475a31089c42701818220d
harsh-groverfk <harsh.grover@flipkart.com>
2017-01-25 13:10:29 +0530

    Signs and verifies files.

--- sign_and_verify.py
+++ sign_and_verify.py
@@ -1,1 +1,19 @@
-print "Signed and verified"
+import requests
+r = requests.post("http://127.0.0.1/reports/sign_now/", files={'report' : open('seventh.py', "r")})
+status_code =  r.status_code
+if(status_code != 200):
+	print "Failed to sign!"
+	exit(1)	
+# Add some extra check on message
+message = r.text
+
+f = open("seventh.gpg", "w")
+f.write(message)
+f.close()
+
+r =  requests.post("http://127.0.0.1:8000/reports/verify/", files={'report' : open('seventh.gpg', "r")})
+print r.text
+# Add some check on message
+if(status_code != 200):
+	print "Failed to verify!"
+	exit(1)
