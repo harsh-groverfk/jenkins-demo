a94ff847655ec56b36b013d80f9c9922a0138da5
harsh-groverfk <harsh.grover@flipkart.com>
2017-01-31 15:11:17 +0530

    test and verify test1

--- sign_and_verify.py
+++ sign_and_verify.py
@@ -1,5 +1,5 @@
 import requests
-r = requests.post("http://127.0.0.1:8000/reports/sign_now/", files={'report' : open('test.txt', "r")})
+r = requests.post("http://127.0.0.1:8000/reports/sign_now/", files={'report' : open('test1.txt', "r")})
 status_code =  r.status_code
 if(status_code != 200):
 	print "Failed to sign!"
@@ -7,11 +7,11 @@ if(status_code != 200):
 # Add some extra check on message
 message = r.text
 
-f = open("test.gpg", "w")
+f = open("test1.gpg", "w")
 f.write(message)
 f.close()
 
-r =  requests.post("http://127.0.0.1:8000/reports/verify/", files={'report' : open('test.gpg', "r")})
+r =  requests.post("http://127.0.0.1:8000/reports/verify/", files={'report' : open('test1.gpg', "r")})
 print r.text
 # Add some check on message
 if(status_code != 200):
